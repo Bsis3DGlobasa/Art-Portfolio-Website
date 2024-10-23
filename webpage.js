02354feb9093f865edac0ed80789944cdbc6c2a5
Bsis3DGlobasa <astra191816@gmail.com>
2024-10-23 14:37:45 +0800

    Your commit message here

---
+++ webpage.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slides img');
+    currentIndex += direction;
+    
+    if (currentIndex < 0) {
+         currentIndex = slides.length - 1;
+         } else if (currentIndex >= slides.length) {
+            currentIndex = 0;
+         }
+
+         const offset = -currentIndex * 100;
+         document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+
+
+    }
